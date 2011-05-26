8fd2b14bf9c70967967ba828cf0b7b26f457f71d
Felipe Oliveira Carvalho <felipekde@gmail.com>
2011-05-25 22:19:32 -0300

    Adicionei um README básico

---
+++ README.mk
@@ -0,0 +1,5 @@
+# FD-X
+
+FD-X é uma implementação da arquitetura Neander-X. O FD-X foi meu trabalho na
+disciplina `Arquitetura de Computadores` durante minha graduação na
+`Universidade Federal de Sergipe`.
