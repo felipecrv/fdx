d72d704fe52f437aadbde389151b2297262ca0eb
Felipe Oliveira Carvalho <felipekde@gmail.com>
2011-05-26 01:13:42 -0300

    Melhorando o README
    Mais informações sobre o Neander

--- README.mk
+++ README.mk
@@ -1,5 +1,16 @@
 # FD-X
 
-FD-X é uma implementação da arquitetura Neander-X. O FD-X foi meu trabalho na
-disciplina `Arquitetura de Computadores` durante minha graduação na
-`Universidade Federal de Sergipe`.
+FD-X é um emulador da arquitetura Neander com algumas extensões. A arquitetura
+Neander é muito simples e é usada para fins didáticos. Sua definição pode ser
+encontrada em [1].
+
+O FD-X foi meu trabalho na disciplina `Arquitetura de Computadores` durante
+minha graduação na `Universidade Federal de Sergipe`.
+
+# Extensões do FD-X
+
+- Subtração - A instrução SUB
+
+
+[1] WEBER, R. F. Fundamentos de Arquitetura de Computadores. 2. Ed. Porto
+Alegre: Instituto de Informática da UFRGS: Sagra Luzzatto, 2001.
